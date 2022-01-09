b3360288fcbc9470e86c791acd66af945fa7ca7b
NullZeal <nullzeal@pm.me>
2022-01-09 11:57:16 -0500

    Adding 5 first exercices

---
+++ ex3.py
@@ -0,0 +1,28 @@
+print "I will now cout my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 -7
+
+print "What is 3 + 2?",3 + 2
+
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is is greater?", 5 > -2
+
+print "Is is greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print 3 + 4 , 3 * 4.2
+print 10001 % 100
+print 7/4 , 7.0 / 4.0
