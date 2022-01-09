bb3c143b8441f9fba45faeff49ac99f56f25060e
NullZeal <nullzeal@pm.me>
2022-01-09 17:36:39 -0500

    Finished ex.12

---
+++ ex12.py
@@ -0,0 +1,8 @@
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+favorite_color = raw_input("What\'s your favorite color?")
+
+print "So, you're %s old, %s tall, %s heavy and your favorite color is %s." % (age, height, weight, favorite_color)
+
+#Checked python -m pydoc raw_input and so on. Interesting read.
