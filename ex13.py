63063314891c93e0cea3115039f8b7ba19e79f14
NullZeal <nullzeal@pm.me>
2022-01-09 18:45:57 -0500

    Finished ex13
    Learned what argv is.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+
+cat_name = raw_input("What's your cat's name?\n")
+
+print "\nYou cat name is:", cat_name
