801f79d6ee43c0d19e653fc9aba50319cd21af4c
NullZeal <nullzeal@pm.me>
2022-01-09 17:02:49 -0500

    Finished ex10

--- ex10.py
+++ ex10.py
@@ -1,3 +1,25 @@
 #Test
 
-print "I am 6'2\" tall."
+print "I am 6'2\" tall." #escape double-quote inside string
+print 'I am 6\'2" tall.' #escape single-quote inside string
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do%s a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+""" % "\r"
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#while False:
+#   for i in ["/", "-", "|", "\\", "|"]:
+#      print "%s\r" % i,
+
+print "Let\'s go mon cher %s" % "what\'s up!"
