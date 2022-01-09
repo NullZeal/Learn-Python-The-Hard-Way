2c79dd4f2128cce48a137471d06d91d0c1fc0bab
NullZeal <nullzeal@pm.me>
2022-01-09 17:24:19 -0500

    Renamed exercices + finished ex 11

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print "How old are you?",
+age = raw_input()
+
+print "How tall are you?",
+height = raw_input()
+
+weight = raw_input("How much do you weight?")
+
+favorite_color = raw_input("What\'s your favorite color?")
+
+print "So, you're %s old, %s tall, %s heavy and your favorite color is %s." % (age, height, weight, favorite_color)
+
