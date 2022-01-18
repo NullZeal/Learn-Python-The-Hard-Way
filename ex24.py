83e3dc123e062c8589d04b0c06f35ecf44676949
NullZeal <nullzeal@pm.me>
2022-01-18 14:13:03 -0500

    Finished exercices 22-23

---
+++ ex24.py
@@ -0,0 +1,14 @@
+#       Comment tag                                         Used to write comments
+%       Substitution symbol                                 Used in strings to indicate to print a variable
+"       String symbol                                       Used to enclose strings 
+'       2nd character symbol                                Also used to enclose strings
+()      Container for parameters                            Contains parameters
+def     Function creation symbol                            Used to create functions
+print   Function to display on the console                  Displays on console
+return  Exit of the functions                               Defines what is given back by the function              
+=       Assignator symbol                                   Used to assign values to variables
+,       Tuple creator                                       Assigns values to multiple variables?
+:       Indicates a switch to the content of a container    Functions, for if / else statements        
+if
+else
+elif
